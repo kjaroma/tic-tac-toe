a0df1c7f19cd72273ae8c3bc4fa460f790a239bc
Krzysztof Jaroma <krzysztof.jaroma@gmail.com>
2024-01-08 19:24:15 +0100

    Fix auth handler

--- server/src/bootstrap/auth/auth.handler.ts
+++ server/src/bootstrap/auth/auth.handler.ts
@@ -3,7 +3,7 @@ import { ApiError } from '../../common/errors';
 import { ErrorMessages, HttpStatus } from '../../common/constants';
 import { FastifyJWT } from '@fastify/jwt';
 
-export function authHandler(req: FastifyRequest) {
+export async function authHandler(req: FastifyRequest) {
   const token = req.cookies.access_token;
   if (!token) {
     throw new ApiError(
