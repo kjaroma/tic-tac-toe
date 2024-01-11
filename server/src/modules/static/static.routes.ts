c7ce643210e3139f07d74bd01df376827dbcad6a
Krzysztof Jaroma <krzysztof.jaroma@gmail.com>
2024-01-11 03:39:00 +0100

    Fix static routing

--- server/src/modules/static/static.routes.ts
+++ server/src/modules/static/static.routes.ts
@@ -1,9 +1,19 @@
 import fastifyStatic from '@fastify/static';
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import path from 'path';
 
 export async function staticRoutes(app: FastifyInstance) {
   app.register(fastifyStatic, {
     root: path.join(process.cwd(), 'public'),
   });
+  function handler(req: FastifyRequest, reply: FastifyReply) {
+    reply.sendFile('index.html')
+  }
+  ['/login', '/register', '/game', '/history'].forEach(url => {
+    app.route({
+      method: ['GET'],
+      url,
+      handler,
+    })
+  })
 }
